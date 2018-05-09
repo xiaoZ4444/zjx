ab80fd8d9b0dcca16a8a289995138934e81fd466
xiaoZ4444 <1196262358@qq.com>
2018-05-09 18:04:09 +0800

    zjx

--- src/main/java/com/zjx/App.java
+++ src/main/java/com/zjx/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Java,你他娘的真难学!" );
+        System.out.println( "Hello Java,你他娘的真难学!阿萨德生产" );
     }
 }
