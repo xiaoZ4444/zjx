3c4e89610400e6fae6d0bb70301e2b4f73924adb
xiaoZ4444 <1196262358@qq.com>
2018-05-09 17:31:40 +0800

    Initial commit

---
+++ src/main/java/com/zjx/App.java
@@ -0,0 +1,13 @@
+package com.zjx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Java,你他娘的真难学!" );
+    }
+}
